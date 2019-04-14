b5e270744e4310da4aa2bfb94c75d225896048d6
Giuseppe Caponetto <giuseppe.caponetto@geoimpact.ch>
2019-04-14 13:06:24 +0200

    wip

--- unity-messaging/http.js
+++ unity-messaging/http.js
@@ -1,14 +1,29 @@
-const express = require('express');
+import express from 'express';
+
+const bodyParser = require('body-parser');
 
 const app = express();
-export default function run(cb) {
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+export default function listen(onMessage) {
   app.get('/', (req, res) => {
-    console.log('got request', { req, res });
-    res.send('Hello World!');
-    cb({ req, res });
+    // console.log('got get request', { req, res });
+    res.send('Hello World (get)!');
+    onMessage({ req, res });
+  });
+
+  app.post('/', (req, res) => {
+    // console.log('got post request', { req, res });
+    res.send('Hello World (post)!');
+    onMessage({ req, res });
   });
 
   app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
   });
 }
+
+// listen((data) => {console.log(data)});
